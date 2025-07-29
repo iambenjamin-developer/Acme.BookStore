703a01685587d31de94d0d9bcedeebc44842c507
Benjamin Correa <iambenjamin.developer@gmail.com>
2025-07-28 21:05:24 -0300

    Add book application Service and DTOs

---
+++ src/Acme.BookStore.Application.Contracts/Books/IBookAppService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Acme.BookStore.Books
+{
+    public interface IBookAppService : ICrudAppService<BookDto, Guid, PagedAndSortedResultRequestDto, CreateUpdateBookDto>
+    {
+        /*
+        ICrudAppService => Defines CRUD methods for the Book entity.
+        BookDto => Data transfer object for show Books.
+        Guid => Primary key type of the Book entity.
+        PagedAndSortedResultRequestDto => Used for paging and sorting requests.
+        CreateUpdateBookDto => Used to create or update a Book entity.
+        */
+    }
+}
---
+++ src/Acme.BookStore.Application/Books/BookAppService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace Acme.BookStore.Books
+{
+    public class BookAppService : CrudAppService<Book, BookDto, Guid, PagedAndSortedResultRequestDto, CreateUpdateBookDto>, IBookAppService
+    {
+        /*
+        CrudAppService => Defines CRUD methods for the Book entity.
+        Book => Domain entity representing a Book.
+        BookDto => Data transfer object for show Books.
+        Guid => Primary key type of the Book entity.
+        PagedAndSortedResultRequestDto => Used for paging and sorting requests.
+        CreateUpdateBookDto => Used to create or update a Book entity.
+        IBookAppService => Implement the IBookAppService
+        */
+        public BookAppService(IRepository<Book, Guid> repository)
+            : base(repository)
+        {
+
+        }
+    }
+}
